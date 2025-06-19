39a92a5d9b0a9c32c70072a3c1cd6d18038c07ec
Ahmad Wandi <84662810+Mayumiwandi@users.noreply.github.com>
2025-06-19 12:15:08 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,9 @@ use tokio_native_tls::TlsConnector as TokioTlsConnector; // Konektor TLS async
 
 const IP_RESOLVER: &str = "speed.cloudflare.com";
 const PATH_RESOLVER: &str = "/meta";
-const PROXY_FILE: &str = "Data/IPProxy19-6.txt"; //input
+const PROXY_FILE: &str = "Data/ProxyIP19-6.txt"; //input
 const OUTPUT_FILE: &str = "Data/alive.txt";
-const MAX_CONCURRENT: usize = 100;
+const MAX_CONCURRENT: usize = 120;
 const TIMEOUT_SECONDS: u64 = 5;
 
 // Define a custom error type that implements Send + Sync
